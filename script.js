302eb4cf14ca71f5df76fcdacb9e5d5b558f461f
ayalaMarmerosh <am0548505474@gmail.com>
2023-10-31 13:02:26 +0200

    Added readme

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const itemForm=document.getElementById('item-form');
 const itemInput=document.getElementById('item-input');
 const itemList=document.getElementById('item-list');
 
+function remove(){
+    alert('c');
+}
+
 function addItem(e){
     e.preventDefault();
     const newItem=itemInput.value;
